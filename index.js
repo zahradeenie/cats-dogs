21214fd6e85cad7588635075f10766270d552fc7
Zahra Deen <zahradeenie@gmail.com>
2019-11-27 14:47:19 +0100

    changed the full styling of the page. from grid to flex

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const catCounterButton = document.getElementByClass("cat-counter");
+
+document.catCounterButton.addEventListener()
+
+function catVote() {
+    catCounter(onclick) 
+    return catCounter;
+};
