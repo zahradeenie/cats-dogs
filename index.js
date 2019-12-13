f312088eb0b8eb4e893db63c35fbaa0f8b2b8589
Zahra Deen <zahradeenie@gmail.com>
2019-12-13 01:37:15 +0100

    functionality to close modal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let catVote = 0;
 let dogVote = 0;
 
-
+// Cat love counter
 const catButton = document.querySelector(".cats button");
 let catCounter = document.querySelector(".cats h2");
 
@@ -10,6 +10,7 @@ catButton.addEventListener("click", () => {
     catCounter.textContent = catVote;
 });
 
+// Dog love counter
 const dogButton = document.querySelector(".dogs button");
 let dogCounter = document.querySelector(".dogs h2");
 
@@ -18,11 +19,14 @@ dogButton.addEventListener("click", () => {
     dogCounter.textContent = dogVote;
 });
 
+// Functionality of modal popup for secret button
 const hateButton = document.querySelector(".hate-button");
-const hateModal = document.querySelector(".hate-modal");
 const modalBg = document.querySelector(".modal-bg");
 
 hateButton.addEventListener("click", () => {
     modalBg.classList.add("is-visible");
 });
 
+modalBg.addEventListener("click", () => {
+    modalBg.classList.remove("is-visible");
+})
