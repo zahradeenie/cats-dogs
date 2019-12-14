2e3471d1220503b3a65faad161bff23fbc74bc71
Zahra Deen <zahradeenie@gmail.com>
2019-12-15 00:18:12 +0100

    new sounds

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function playCatSound() {
     let audio = document.querySelectorAll("audio[data-sound='cat']");
     for (let sound of audio) {
         sound.play();
-    }
-};
+    };
+}
 
 catButton.addEventListener("click", () => {
     catVote += 1;
@@ -49,8 +49,12 @@ dogButton.addEventListener("click", () => {
 const hateButton = document.querySelector(".hate-button");
 const modalBg = document.querySelector(".modal-bg");
 
+let audio = document.querySelectorAll("audio[data-sound='what']");
+
+
 hateButton.addEventListener("click", () => {
     modalBg.classList.add("is-visible");
+    audio.play();
 });
 
 modalBg.addEventListener("click", () => {
